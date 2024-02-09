12cc1d3b737ec3c6ae403e34f2c2dd9ba72a84a3
Maverick _Unit #100011000 <parisshakan@gmail.com>
2024-02-08 20:05:50 -0500

    Week 3 class update
    Did some stuff in class about startPos, transform.position, Vector3

--- Assets/Scripts/KingMovement.cs
+++ Assets/Scripts/KingMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class KingMovement : MonoBehaviour
 {
+    public Transform hazard; 
+    Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position == hazard.position){
+            Debug.Log("HAZARD OUCH");
+            transform.position = startPos;
+        }
         if (Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("w key was pressed");
@@ -31,13 +38,12 @@ public class KingMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("d key was pressed");
-            transform.position += new Vector3(1f, 0f, 0f);
-            
+            transform.position += new Vector3(1f, 0f, 0f);  
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("space key was pressed");
-            transform.position += new Vector3(0f, 1f, 0f);
+            transform.position += new Vector3(0f, 1.5f, 0f);
         }
     }
 }
--- Assets/Scripts/QueenMovement.cs
+++ Assets/Scripts/QueenMovement.cs
@@ -4,40 +4,46 @@ using UnityEngine;
 
 public class QueenMovement : MonoBehaviour
 {
+    public Transform hazard; 
+    Vector3 startPos;
+    
     // Start is called before the first frame update
     void Start()
     {
-       
+       startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.W))
+        if (transform.position == hazard.position){
+            Debug.Log("HAZARD OUCH");
+            transform.position = startPos;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("w key was pressed");
-            transform.position += new Vector3(0f, 0f, -1f);
+            transform.position -= new Vector3(0f, 0f, 1f);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("s key was pressed");
-            transform.position += new Vector3(0f, 0f, 1f);
+            transform.position -= new Vector3(0f, 0f, -1f);
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("a key was pressed");
-            transform.position += new Vector3(1f, 0f, 0f);
+            transform.position -= new Vector3(-1f, 0f, 0f);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("d key was pressed");
-            transform.position += new Vector3(-1f, 0f, 0f);
-            
+            transform.position -= new Vector3(1f, 0f, 0f);  
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("space key was pressed");
-            transform.position += new Vector3(0f, 1f, 0f);
-        }  
+            transform.position += new Vector3(0f, 1.5f, 0f);
+        } 
     }
 }

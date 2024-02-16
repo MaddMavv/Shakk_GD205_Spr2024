f53c6df8d9a83a9ae97edbb63c713fb4286e65bd
Maverick _Unit #100011000 <parisshakan@gmail.com>
2024-02-15 19:52:20 -0500

    Week 3 & 4 stuff

--- Assets/Scripts/QueenMovement.cs
+++ Assets/Scripts/QueenMovement.cs
@@ -5,44 +5,55 @@ using UnityEngine;
 public class QueenMovement : MonoBehaviour
 {
     public Transform hazard; 
+    public Transform[] hazards;
+    AudioSource deadAudioPlayer;
+    public AudioClip dadAudioClip;
     Vector3 startPos;
     
     // Start is called before the first frame update
     void Start()
     {
-       startPos = transform.position;
+        startPos = transform.position;
+        deadAudioPlayer = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0;i < hazards.Length; i++){
+            if (transform.position == hazards[i].position){
+                transform.position = startPos;
+
+                deadAudioPlayer.PlayOneShot(dadAudioClip, 0.5f);
+            }
+        }
         if (transform.position == hazard.position){
-            Debug.Log("HAZARD OUCH");
+            Debug.Log("QUEEN IS HURT");
             transform.position = startPos;
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            Debug.Log("w key was pressed");
+            Debug.Log("Queen moved forwards");
             transform.position -= new Vector3(0f, 0f, 1f);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Debug.Log("s key was pressed");
+            Debug.Log("Queen moved backwards");
             transform.position -= new Vector3(0f, 0f, -1f);
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Debug.Log("a key was pressed");
+            Debug.Log("Queen moved left");
             transform.position -= new Vector3(-1f, 0f, 0f);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            Debug.Log("d key was pressed");
+            Debug.Log("Queen moved right");
             transform.position -= new Vector3(1f, 0f, 0f);  
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("space key was pressed");
+            Debug.Log("Queen jumped");
             transform.position += new Vector3(0f, 1.5f, 0f);
         } 
     }

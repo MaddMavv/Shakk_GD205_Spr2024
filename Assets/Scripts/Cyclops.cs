4cdb6b4476b1e499e44ee47299cf3ab7b39d1c4b
Maverick _Unit #100011000 <parisshakan@gmail.com>
2024-03-14 20:17:51 -0400

    Week 6 stuff
    blah blah

---
+++ Assets/Scripts/Cyclops.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cyclops : MonoBehaviour
+{
+   public float explosionForce = 5f;
+   public float explosionRadius = 20f;
+   public 
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            
+            if(Physics.Raycast(laser, out hit) && Input.GetMouseButton(0)){
+                Debug.Log("booyah, cyclops sucks!");
+                if (hit,rigidbody){
+                    hit.rigidbody.AddExplosionForce(explosionForce, hit.point, explosionRadius);
+                }
+            }
+
+            if(Physics.Raycast(laser, out hit) && Input.GetMouseButton(1)){  
+                Instantiate(prefab, hit.point, Quaternion, ) 
+        }
+    }
+}
